e562ed5b5b93327bf3a37fd5d098906f7a763361
mjensen <mjensen@atlassian.com>
2008-05-30 04:04:17 +0000

    [maven-release-plugin]  copy for tag atlassian-plugins-0.21
    git-svn-id: https://studio.atlassian.com/svn/PLUG/tags/atlassian-plugins-0.21@22338 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ src/main/java/com/atlassian/plugin/classloader/DelegationClassLoader.java
@@ -0,0 +1,74 @@
+package com.atlassian.plugin.classloader;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * A class loader that delegates to another class loader.
+ */
+public class DelegationClassLoader extends ClassLoader
+{
+    private static final Log log = LogFactory.getLog(DelegationClassLoader.class);
+
+    private ClassLoader delegateClassLoader;
+
+    public void setDelegateClassLoader(ClassLoader delegateClassLoader)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("Update class loader delegation from [" + this.delegateClassLoader +
+                    "] to [" + delegateClassLoader + "]");
+        }
+        this.delegateClassLoader = delegateClassLoader;
+    }
+
+    public Class loadClass(String name) throws ClassNotFoundException
+    {
+        return delegateClassLoader != null ? delegateClassLoader.loadClass(name) : null;
+    }
+
+    public URL getResource(String name)
+    {
+        return delegateClassLoader != null ? delegateClassLoader.getResource(name) : null;
+    }
+
+    public InputStream getResourceAsStream(String name)
+    {
+        return delegateClassLoader != null ? delegateClassLoader.getResourceAsStream(name) : null;
+    }
+
+    public synchronized void setDefaultAssertionStatus(boolean enabled)
+    {
+        if (delegateClassLoader != null)
+        {
+            delegateClassLoader.setDefaultAssertionStatus(enabled);
+        }
+    }
+
+    public synchronized void setPackageAssertionStatus(String packageName, boolean enabled)
+    {
+        if (delegateClassLoader != null)
+        {
+            delegateClassLoader.setPackageAssertionStatus(packageName, enabled);
+        }
+    }
+
+    public synchronized void setClassAssertionStatus(String className, boolean enabled)
+    {
+        if (delegateClassLoader != null)
+        {
+            delegateClassLoader.setClassAssertionStatus(className, enabled);
+        }
+    }
+
+    public synchronized void clearAssertionStatus()
+    {
+        if (delegateClassLoader != null)
+        {
+            delegateClassLoader.clearAssertionStatus();
+        }
+    }
+}
