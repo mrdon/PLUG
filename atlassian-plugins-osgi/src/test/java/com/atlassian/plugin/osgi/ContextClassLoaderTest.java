f2e7dfc80c4ca9027560b450bca9e55dd0ea535a
dbrown <dbrown@atlassian.com>
2008-07-10 23:57:18 +0000

    Starting on an integration test framework
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@22473 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugins-osgi/src/test/java/com/atlassian/plugin/osgi/ContextClassLoaderTest.java
@@ -0,0 +1,16 @@
+package com.atlassian.plugin.osgi;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mrdon
+ * Date: 10/07/2008
+ * Time: 10:50:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContextClassLoaderTest extends PluginInContainerTestBase {
+
+    public void testCorrectContextClassLoaderForHostComponents()
+    {
+            
+    }
+}
