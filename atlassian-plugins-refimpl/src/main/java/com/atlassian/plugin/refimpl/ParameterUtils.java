48f15a7b53d19d3d29726d1fb96ff937eaa071f7
rwallace <rwallace@atlassian.com>
2008-09-23 18:05:22 +0000

    get the base url from a system property
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@22712 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-refimpl/src/main/java/com/atlassian/plugin/refimpl/ParameterUtils.java
+++ atlassian-plugins-refimpl/src/main/java/com/atlassian/plugin/refimpl/ParameterUtils.java
@@ -1,12 +1,9 @@
 package com.atlassian.plugin.refimpl;
 
-public class ParameterUtils {
-
-    public static String getBaseUrl() {
-        String port = System.getProperty("http.port");
-        if (port == null) {
-            port = "8080";
-        }
-        return "http://localhost:"+port+"/atlassian-plugins-refimpl";
+public class ParameterUtils
+{
+    public static String getBaseUrl()
+    {
+        return System.getProperty("baseurl", "http://localhost:8080/atlassian-plugins-refimpl");
     }
 }
