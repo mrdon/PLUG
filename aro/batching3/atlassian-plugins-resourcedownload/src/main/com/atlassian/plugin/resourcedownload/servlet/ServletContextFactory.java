c99df1626690453f386f3ee3f46939a92f1e165d
aro <aro@atlassian.com>
2008-10-20 07:43:53 +0000

    reverting commit
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/private@22822 2c54a935-e501-0410-bc05-97a93f6bca70

--- aro/batching3/atlassian-plugins-resourcedownload/src/main/com/atlassian/plugin/resourcedownload/servlet/ServletContextFactory.java
+++
@@ -1,8 +0,0 @@
-package com.atlassian.plugin.resourcedownload.servlet;
-
-import javax.servlet.ServletContext;
-
-public interface ServletContextFactory
-{
-    public ServletContext getServletContext();
-}
