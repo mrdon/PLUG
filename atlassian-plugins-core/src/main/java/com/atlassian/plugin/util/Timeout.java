1ca0be7a69b7d23f0da22039e7cb3cc80f19c62b
jwesleysmith <jwesleysmith@atlassian.com>
2008-11-12 06:36:11 +0000

    Added more flexible invoke method that does not hard-code single second wait granularity.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@23263 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/Timeout.java
@@ -0,0 +1,118 @@
+package com.atlassian.plugin.util;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Used to calculate elapsed time for timeouts from when it is created when successively calling
+ * blocking methods. Always converts to nanoseconds.
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * Timeout timeout = Timeout.getNanosTimeout(1, TimeUnit.SECONDS);
+ * String str = futureString.get(timeout.getTime(), timeout.getUnit());
+ * Integer num = futureInt.get(timeout.getTime(), timeout.getUnit());
+ * </pre>
+ * 
+ * where if the first call takes quarter of a second, the second call is passed the equivalent of
+ * three-quarters of a second.
+ */
+public class Timeout
+{
+    private static final TimeSupplier NANO_SUPPLIER = new TimeSupplier()
+    {
+        public long currentTime()
+        {
+            return System.nanoTime();
+        };
+
+        public TimeUnit precision()
+        {
+            return TimeUnit.NANOSECONDS;
+        };
+    };
+
+    private static final TimeSupplier MILLIS_SUPPLIER = new TimeSupplier()
+    {
+        public long currentTime()
+        {
+            return System.currentTimeMillis();
+        };
+
+        public TimeUnit precision()
+        {
+            return TimeUnit.MILLISECONDS;
+        };
+    };
+
+    /**
+     * Get a {@link Timeout} that uses nanosecond precision. The accuracy will depend on the
+     * accuracy of {@link System#nanoTime()}.
+     * 
+     * @param time the maximum time to wait for the lock
+     * @param unit the time unit of the <tt>time</tt> argument.
+     * @return timeout with {@link TimeUnit#NANOSECONDS} precision.
+     */
+    public static Timeout getNanosTimeout(final long time, final TimeUnit unit)
+    {
+        return new Timeout(time, unit, NANO_SUPPLIER);
+    }
+
+    /**
+     * Get a {@link Timeout} that uses millisecond precision.
+     * 
+     * @param time the maximum time to wait for the lock
+     * @param unit the time unit of the <tt>time</tt> argument.
+     * @return timeout with {@link TimeUnit#MILLISECONDS} precision.
+     */
+    public static Timeout getMillisTimeout(final long time, final TimeUnit unit)
+    {
+        return new Timeout(time, unit, MILLIS_SUPPLIER);
+    }
+
+    private final long created;
+    private final long time;
+    private final TimeSupplier supplier;
+
+    Timeout(final long time, final TimeUnit unit, final TimeSupplier supplier)
+    {
+        created = supplier.currentTime();
+        this.supplier = supplier;
+        this.time = this.supplier.precision().convert(time, unit);
+    }
+
+    public long getTime()
+    {
+        return (created + time) - supplier.currentTime();
+    }
+
+    public TimeUnit getUnit()
+    {
+        return supplier.precision();
+    }
+
+    public String getRemaining()
+    {
+        return getTime() + " " + getUnit() + " remaining";
+    }
+
+    /**
+     * Has this timeout expired
+     * 
+     * @return
+     */
+    public boolean isExpired()
+    {
+        return getTime() <= 0;
+    }
+
+    /**
+     * Supply time and precision to a {@link Timeout}.
+     */
+    interface TimeSupplier
+    {
+        long currentTime();
+
+        TimeUnit precision();
+    }
+}
--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/WaitUntil.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/WaitUntil.java
@@ -1,9 +1,11 @@
 package com.atlassian.plugin.util;
 
+import static java.lang.Thread.sleep;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import static java.lang.Thread.sleep;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Utility methods for synchronising on asynchronous processes
@@ -12,7 +14,8 @@ public class WaitUntil
 {
     private static final Log log = LogFactory.getLog(WaitUntil.class);
 
-    private WaitUntil() {}
+    private WaitUntil()
+    {}
 
     /**
      * Invokes the wait condition, trying every second for 10 seconds
@@ -20,7 +23,7 @@ public class WaitUntil
      * @param waitCondition The condition that determines when to stop waiting
      * @return True if the condition returned true
      */
-    public static boolean invoke(WaitCondition waitCondition)
+    public static boolean invoke(final WaitCondition waitCondition)
     {
         return invoke(waitCondition, 10);
     }
@@ -32,10 +35,26 @@ public class WaitUntil
      * @param tries The number of tries to attempt
      * @return True if the condition returned true
      */
-    public static boolean invoke(WaitCondition waitCondition, int tries)
+    public static boolean invoke(final WaitCondition waitCondition, final int tries)
     {
+        final int secondMillis = 1000;
+        return invoke(waitCondition, tries * secondMillis, TimeUnit.MILLISECONDS, secondMillis);
+    }
+
+    /**
+     * Invokes the wait condition, trying every second for the configured seconds
+     *
+     * @param waitCondition The condition that determines when to stop waiting
+     * @param time the amount of time to wait
+     * @param unit the time unit time is specified in
+     * @param retryInterval how often to re-check the condition (specified in the supplied TimeUnit)
+     * @return True if the condition returned true
+     */
+    public static boolean invoke(final WaitCondition waitCondition, final int time, final TimeUnit unit, final int retryInterval)
+    {
+        final Timeout timeout = Timeout.getMillisTimeout(time, unit);
         boolean successful = false;
-        for (int count = tries; count > 0; count--)
+        while (!timeout.isExpired())
         {
             if (waitCondition.isFinished())
             {
@@ -45,24 +64,24 @@ public class WaitUntil
 
             if (log.isInfoEnabled())
             {
-                log.info(waitCondition.getWaitMessage() + ", " + count + " tries remaining");
+                log.info(waitCondition.getWaitMessage() + ", " + timeout.getRemaining());
             }
             try
             {
-                sleep(1000);
-            } catch (InterruptedException e)
+                sleep(unit.toMillis(retryInterval));
+            }
+            catch (final InterruptedException e)
             {
                 break;
             }
         }
         return successful;
-
     }
 
     /**
      * The condition to determine when to stop waiting
      */
-    public static interface WaitCondition
+    public interface WaitCondition
     {
         /**
          * If the condition has been finished
