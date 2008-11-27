590d6dea6dea952fd07ba0d439ef78006634c9ff
dbrown <dbrown@atlassian.com>
2008-11-27 08:52:56 +0000

    Choosing a bit more sane defaults for package config
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@23933 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/container/impl/DefaultPackageScannerConfiguration.java
+++ atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/container/impl/DefaultPackageScannerConfiguration.java
@@ -13,7 +13,7 @@ public class DefaultPackageScannerConfiguration implements PackageScannerConfigu
 
     private List<String> jarIncludes = Arrays.asList("*.jar");
     private List<String> jarExcludes = Collections.emptyList();
-    private List<String> packageIncludes = Arrays.asList("com.atlassian.*", "javax.swing.tree", "org.quartz", "org.quartz.*", "bucket.*", "net.sf.cglib", "net.sf.cglib.*", "net.sf.hibernate", "net.sf.hibernate.*", "com.octo.captcha.*", "com.opensymphony.*", "org.apache.*", "org.xml.*", "javax.*", "org.w3c.*");
+    private List<String> packageIncludes = Arrays.asList("com.atlassian.*", "org.apache.*", "org.xml.*", "javax.*", "org.w3c.*");
     private List<String> packageExcludes = Arrays.asList("com.springframework*", "org.apache.commons.logging*");
     private Map<String, String> packageVersions;
     private String hostVersion;
@@ -35,6 +35,7 @@ public class DefaultPackageScannerConfiguration implements PackageScannerConfigu
         jarExcludes = new ArrayList<String>(jarExcludes);
         packageIncludes = new ArrayList<String>(packageIncludes);
         packageExcludes = new ArrayList<String>(packageExcludes);
+        packageVersions = new HashMap<String,String>();
     }
 
 
