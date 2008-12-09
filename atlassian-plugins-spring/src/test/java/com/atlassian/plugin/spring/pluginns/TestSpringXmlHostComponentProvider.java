1403d5238517a883393693d4320f2d829f0eadb6
dbrown <dbrown@atlassian.com>
2008-12-09 09:13:31 +0000

    Fixing error when declaring multiple interfaces PLUG-229
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/atlassian-plugins-2.1.x@24536 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-spring/src/test/java/com/atlassian/plugin/spring/pluginns/TestSpringXmlHostComponentProvider.java
+++ atlassian-plugins-spring/src/test/java/com/atlassian/plugin/spring/pluginns/TestSpringXmlHostComponentProvider.java
@@ -4,15 +4,18 @@ import junit.framework.TestCase;
 import org.springframework.beans.factory.support.StaticListableBeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import com.atlassian.plugin.spring.SpringHostComponentProvider;
 import com.atlassian.plugin.spring.AvailableToPlugins;
 import com.atlassian.plugin.spring.Fooable;
+import com.atlassian.plugin.spring.Barable;
 import com.atlassian.plugin.osgi.hostcomponents.impl.DefaultComponentRegistrar;
 import com.atlassian.plugin.osgi.hostcomponents.HostComponentRegistration;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class TestSpringXmlHostComponentProvider extends TestCase
 {
@@ -48,10 +51,29 @@ public class TestSpringXmlHostComponentProvider extends TestCase
 
         List<HostComponentRegistration> list = registrar.getRegistry();
         assertNotNull(list);
-        assertEquals(1, list.size());
+        assertEquals(2, list.size());
         assertEquals("foo", list.get(0).getProperties().get("bean-name"));
         assertEquals(1, list.get(0).getMainInterfaces().length);
         assertEquals(BeanFactoryAware.class.getName(), list.get(0).getMainInterfaces()[0]);
+    }
+
+    public void testProvideWithMultipleCustomInterfaces()
+    {
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("com/atlassian/plugin/spring/pluginns/plugins-spring-test-interface.xml"));
+
+        SpringXmlHostComponentProvider provider = (SpringXmlHostComponentProvider) factory.getBean(SpringXmlHostComponentProvider.HOST_COMPONENT_PROVIDER);
+        assertNotNull(provider);
+
+
+        DefaultComponentRegistrar registrar = new DefaultComponentRegistrar();
+        provider.provide(registrar);
+
+        List<HostComponentRegistration> list = registrar.getRegistry();
+        assertNotNull(list);
+        assertEquals("fooMultipleInterface", list.get(1).getProperties().get("bean-name"));
+        assertEquals(2, list.get(1).getMainInterfaces().length);
+        assertTrue(Arrays.asList(list.get(1).getMainInterfaces()).contains(BeanFactoryAware.class.getName()));
+        assertTrue(Arrays.asList(list.get(1).getMainInterfaces()).contains(Barable.class.getName()));
     }
 
     public void testProvideWithNestedContexts()
