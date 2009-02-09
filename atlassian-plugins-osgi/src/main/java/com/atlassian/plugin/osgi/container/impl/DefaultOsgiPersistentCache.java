3d489f36daa1bbbb53b6d627601a02721da2dd55
dbrown <dbrown@atlassian.com>
2009-02-09 00:08:21 +0000

    Rolling back new plugin cache directories PLUG-187
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@27853 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/container/impl/DefaultOsgiPersistentCache.java
@@ -0,0 +1,90 @@
+package com.atlassian.plugin.osgi.container.impl;
+
+import com.atlassian.plugin.osgi.container.OsgiPersistentCache;
+import com.atlassian.plugin.osgi.container.OsgiContainerException;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang.Validate;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Default implementation of persistent cache.  Of the two constructors, {@link #DefaultOsgiPersistentCache(File)} is
+ * recommended to help standardize cache directory names
+ *
+ * @since 2.2.0
+ */
+public class DefaultOsgiPersistentCache implements OsgiPersistentCache
+{
+    private final File osgiBundleCache;
+    private final File frameworkBundleCache;
+    private final File transformedPluginCache;
+
+    public DefaultOsgiPersistentCache(File baseDir)
+    {
+        Validate.isTrue(baseDir.exists(), "The base directory for OSGi persistent caches should exist");
+        osgiBundleCache = new File(baseDir, "felix");
+        frameworkBundleCache = new File(baseDir, "framework-bundles");
+        transformedPluginCache = new File(baseDir, "transformed-plugins");
+        validate();
+    }
+
+    public DefaultOsgiPersistentCache(File osgiBundleCache, File frameworkBundleCache, File transformedPluginCache)
+    {
+        this.osgiBundleCache = osgiBundleCache;
+        this.frameworkBundleCache = frameworkBundleCache;
+        this.transformedPluginCache = transformedPluginCache;
+        validate();
+    }
+
+
+    public File getFrameworkBundleCache()
+    {
+        return frameworkBundleCache;
+    }
+
+    public File getOsgiBundleCache()
+    {
+        return osgiBundleCache;
+    }
+
+    public File getTransformedPluginCache()
+    {
+        return transformedPluginCache;
+    }
+
+    public void clear() throws OsgiContainerException
+    {
+        try
+        {
+            FileUtils.cleanDirectory(frameworkBundleCache);
+            FileUtils.cleanDirectory(osgiBundleCache);
+            FileUtils.cleanDirectory(transformedPluginCache);
+        }
+        catch (IOException e)
+        {
+            throw new OsgiContainerException("Unable to clear OSGi caches", e);
+        }
+    }
+
+    private void validate()
+    {
+        ensureDirectoryExists(frameworkBundleCache);
+        ensureDirectoryExists(osgiBundleCache);
+        ensureDirectoryExists(transformedPluginCache);
+    }
+
+    private void ensureDirectoryExists(File dir)
+    {
+        if (dir.exists() && !dir.isDirectory())
+        {
+            throw new IllegalArgumentException("Directory '"+dir+"' is incorrectly a file");
+        }
+
+        if (!dir.exists() && !dir.mkdir())
+        {
+            throw new IllegalArgumentException("Directory '"+dir+"' cannot be created");
+        }
+    }
+}
