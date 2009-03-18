8c1af1f58c032f1405bcfa01d62f0b19d9cb0df2
jwesleysmith <jwesleysmith@atlassian.com>
2009-03-18 00:14:10 +0000

    InvocationHandler is now a static inner class to remove back reference and has the benefit of being appropriately named - ContextClassLoaderSettingInvocationHandler
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@30693 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/hostcomponents/impl/DefaultComponentRegistrar.java
+++ atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/hostcomponents/impl/DefaultComponentRegistrar.java
@@ -1,40 +1,46 @@
 package com.atlassian.plugin.osgi.hostcomponents.impl;
 
+import com.atlassian.plugin.hostcontainer.HostContainer;
+import com.atlassian.plugin.osgi.hostcomponents.ComponentRegistrar;
+import com.atlassian.plugin.osgi.hostcomponents.ContextClassLoaderStrategy;
+import com.atlassian.plugin.osgi.hostcomponents.HostComponentRegistration;
+import com.atlassian.plugin.osgi.hostcomponents.InstanceBuilder;
+import com.atlassian.plugin.osgi.hostcomponents.PropertyBuilder;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import java.util.*;
-import java.lang.reflect.Proxy;
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
-
-import com.atlassian.plugin.osgi.hostcomponents.*;
-import com.atlassian.plugin.hostcontainer.HostContainer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * Default component registrar that also can write registered host components into the OSGi service registry
+ * Default component registrar that also can write registered host components into the OSGi service registry.
  */
 public class DefaultComponentRegistrar implements ComponentRegistrar
 {
-    private List<HostComponentRegistration> registry = new ArrayList<HostComponentRegistration>();
+    private final List<HostComponentRegistration> registry = new ArrayList<HostComponentRegistration>();
 
-    private Log log = LogFactory.getLog(DefaultComponentRegistrar.class);
+    private final Log log = LogFactory.getLog(DefaultComponentRegistrar.class);
 
-    public InstanceBuilder register(Class<?>... mainInterfaces)
+    public InstanceBuilder register(final Class<?>... mainInterfaces)
     {
-        Registration reg = new Registration(mainInterfaces);
+        final Registration reg = new Registration(mainInterfaces);
         registry.add(reg);
         return new DefaultInstanceBuilder(reg);
     }
 
-    public List<ServiceRegistration> writeRegistry(BundleContext ctx)
+    public List<ServiceRegistration> writeRegistry(final BundleContext ctx)
     {
-        ArrayList<ServiceRegistration> services = new ArrayList<ServiceRegistration>();
+        final ArrayList<ServiceRegistration> services = new ArrayList<ServiceRegistration>();
 
-        for (HostComponentRegistration reg : registry)
+        for (final HostComponentRegistration reg : registry)
         {
             if (Arrays.asList(reg.getMainInterfaceClasses()).contains(HostContainer.class))
             {
@@ -42,7 +48,7 @@ public class DefaultComponentRegistrar implements ComponentRegistrar
                 continue;
             }
 
-            String[] names = reg.getMainInterfaces();
+            final String[] names = reg.getMainInterfaces();
 
             reg.getProperties().put(HOST_COMPONENT_FLAG, Boolean.TRUE.toString());
 
@@ -54,13 +60,17 @@ public class DefaultComponentRegistrar implements ComponentRegistrar
             }
             
             if (log.isDebugEnabled())
-                log.debug("Registering: "+ Arrays.asList(names)+" instance "+reg.getInstance() + "with properties: "+reg.getProperties());
+            {
+                log.debug("Registering: " + Arrays.asList(names) + " instance " + reg.getInstance() + "with properties: " + reg.getProperties());
+            }
 
             Object service = reg.getInstance();
             if (!ContextClassLoaderStrategy.USE_PLUGIN.name().equals(reg.getProperties().get(PropertyBuilder.CONTEXT_CLASS_LOADER_STRATEGY)))
+            {
                 service = wrapService(reg.getMainInterfaceClasses(), reg.getInstance());
+            }
 
-            ServiceRegistration sreg = ctx.registerService(names, service, reg.getProperties());
+            final ServiceRegistration sreg = ctx.registerService(names, service, reg.getProperties());
             if (sreg != null)
             {
                 services.add(sreg);
@@ -69,37 +79,54 @@ public class DefaultComponentRegistrar implements ComponentRegistrar
         return services;
     }
 
-    /**
-     * Wraps the service in a dynamic proxy that ensures all methods are executed with the object class's class loader
-     * as the context class loader
-     * @param interfaces The interfaces to proxy
-     * @param service The instance to proxy
-     * @return A proxy that wraps the service
-     */
-    protected Object wrapService(Class[] interfaces, final Object service)
-    {
-        return Proxy.newProxyInstance(
-            getClass().getClassLoader(),
-            interfaces,
-            new InvocationHandler() {
-                public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
-                    final Thread thread = Thread.currentThread();
-                    final ClassLoader ccl = thread.getContextClassLoader();
-                    try {
-                        thread.setContextClassLoader(service.getClass().getClassLoader());
-                        return method.invoke(service, objects);
-                    } catch (InvocationTargetException e) {
-                        throw e.getTargetException();
-                    } finally {
-                        thread.setContextClassLoader(ccl);
-                    }
-                }
-            }
-        );
-    }
-
     public List<HostComponentRegistration> getRegistry()
     {
         return registry;
+    }
+
+    /**
+     * Wraps the service in a dynamic proxy that ensures all methods are executed with the object class's class loader
+     * as the context class loader
+     *
+     * @param interfaces The interfaces to proxy
+     * @param service    The instance to proxy
+     * @return A proxy that wraps the service
+     */
+    protected Object wrapService(final Class<?>[] interfaces, final Object service)
+    {
+        return Proxy.newProxyInstance(getClass().getClassLoader(), interfaces, new ContextClassLoaderSettingInvocationHandler(service));
+    }
+
+    /**
+     * InvocationHandler for a dynamic proxy that ensures all methods are executed with the
+     * object class's class loader as the context class loader.
+     */
+    private static class ContextClassLoaderSettingInvocationHandler implements InvocationHandler
+    {
+        private final Object service;
+
+        ContextClassLoaderSettingInvocationHandler(final Object service)
+        {
+            this.service = service;
+        }
+
+        public Object invoke(final Object o, final Method method, final Object[] objects) throws Throwable
+        {
+            final Thread thread = Thread.currentThread();
+            final ClassLoader ccl = thread.getContextClassLoader();
+            try
+            {
+                thread.setContextClassLoader(service.getClass().getClassLoader());
+                return method.invoke(service, objects);
+            }
+            catch (final InvocationTargetException e)
+            {
+                throw e.getTargetException();
+            }
+            finally
+            {
+                thread.setContextClassLoader(ccl);
+            }
+        }
     }
 }
