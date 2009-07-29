d072972ca7a8fd7f797293045c501928b1a453a8
dbrown <dbrown@atlassian.com>
2009-07-29 08:53:13 +0000

    Change component descriptor to not try to load the class PLUG-408
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/atlassian-plugins-2.2.x@40828 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/factory/descriptor/ComponentModuleDescriptor.java
+++ atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/factory/descriptor/ComponentModuleDescriptor.java
@@ -1,6 +1,8 @@
 package com.atlassian.plugin.osgi.factory.descriptor;
 
 import com.atlassian.plugin.descriptors.AbstractModuleDescriptor;
+import com.atlassian.plugin.Plugin;
+import com.atlassian.plugin.PluginParseException;
 
 /**
  * Module descriptor for Spring components.  Shouldn't be directly used outside providing read-only information.
@@ -9,9 +11,15 @@ import com.atlassian.plugin.descriptors.AbstractModuleDescriptor;
  */
 public class ComponentModuleDescriptor extends AbstractModuleDescriptor<Void>
 {
+
     public Void getModule()
     {
         throw new UnsupportedOperationException();
     }
 
+    @Override
+    protected void loadClass(Plugin plugin, String clazz) throws PluginParseException
+    {
+        // do nothing
+    }
 }
---
+++ atlassian-plugins-osgi/src/test/java/com/atlassian/plugin/osgi/factory/descriptor/TestComponentModuleDescriptor.java
@@ -0,0 +1,38 @@
+package com.atlassian.plugin.osgi.factory.descriptor;
+
+import junit.framework.TestCase;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyObject;
+import com.atlassian.plugin.Plugin;
+
+public class TestComponentModuleDescriptor extends TestCase
+{
+    public void testEnableDoesNotLoadClass() throws ClassNotFoundException
+    {
+        ComponentModuleDescriptor desc = new ComponentModuleDescriptor();
+
+        Element e = DocumentHelper.createElement("foo");
+        e.addAttribute("key", "foo");
+        e.addAttribute("class", Foo.class.getName());
+
+        Plugin plugin = mock(Plugin.class);
+        when(plugin.<Foo>loadClass((String)anyObject(), (Class<?>) anyObject())).thenReturn(Foo.class);
+        desc.init(plugin, e);
+
+        desc.enabled();
+        assertFalse(Foo.called);
+    }
+
+    public static class Foo
+    {
+        public static boolean called;
+
+        public Foo()
+        {
+            called = true;
+        }
+    }
+}
