470b86fb852b5fed3a9a62ebdf1747b4820e4c44
mlassau <mlassau@atlassian.com>
2009-08-12 02:21:22 +0000

    PLUG-414 Include all "caused by" errors in the NotificationException, and also log them all just in case.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/atlassian-plugins-2.3.x@41697 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/event/NotificationException.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/event/NotificationException.java
@@ -2,15 +2,40 @@ package com.atlassian.plugin.event;
 
 import com.atlassian.plugin.PluginException;
 
+import java.util.List;
+import java.util.Collections;
+
 /**
- * This is used to wrap an exception thrown by a Plugin Event Listener.
+ * This is used to wrap one or more exceptions thrown by Plugin Event Listeners on receiving an event.
  *
- * @since v4.0
+ * <p> {@link #getAllCauses()} will return a list with all the exceptions that were thrown by the listeners.
+ * <p> {@link #getCause()} will return just the first Exception in the list.
+ *
+ * @since v2.3
  */
 public class NotificationException extends PluginException
 {
+    private final List<Throwable> allCauses;
+
     public NotificationException(final Throwable cause)
     {
         super(cause);
+        allCauses = Collections.singletonList(cause);
+    }
+
+    /**
+     * Constructs a NotificationException with a List of the Exceptions that were thrown by the Listeners.
+     * @param causes all Exceptions that were thrown by the Listeners.
+     */
+    public NotificationException(final List<Throwable> causes)
+    {
+        //noinspection ThrowableResultOfMethodCallIgnored
+        super(causes.get(0));
+        this.allCauses = Collections.unmodifiableList(causes);
+    }
+
+    public List<Throwable> getAllCauses()
+    {
+        return allCauses;
     }
 }
