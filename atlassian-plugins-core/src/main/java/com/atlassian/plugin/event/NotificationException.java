81114232fa6cf8759ecaec5fd7a9504baceb06a1
mlassau <mlassau@atlassian.com>
2009-08-13 09:23:29 +0000

    PLUG-414 Fix javadoc
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@41832 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/event/NotificationException.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/event/NotificationException.java
@@ -11,12 +11,19 @@ import java.util.Collections;
  * <p> {@link #getAllCauses()} will return a list with all the exceptions that were thrown by the listeners.
  * <p> {@link #getCause()} will return just the first Exception in the list.
  *
- * @since v2.3
+ * @since 2.3.0
  */
 public class NotificationException extends PluginException
 {
     private final List<Throwable> allCauses;
 
+    /**
+     * Constructs a NotificationException with a single caused by Exception thrown by a Listener.
+     * @param  cause the cause (which is saved for later retrieval by the {@link #getCause()} method).
+     *              (A <code>null</code> value should never be passed because this exception is only used to wrap other exceptions.)
+     * @throws NullPointerException If a null List is passed.
+     * @throws IndexOutOfBoundsException If an empty List is passed.
+     */
     public NotificationException(final Throwable cause)
     {
         super(cause);
@@ -25,7 +32,10 @@ public class NotificationException extends PluginException
 
     /**
      * Constructs a NotificationException with a List of the Exceptions that were thrown by the Listeners.
+     *
      * @param causes all Exceptions that were thrown by the Listeners.
+     *          (the full list will be available by the {@link #getAllCauses()} method;
+     *          the {@link #getCause()} method will just return the first cause in the list.
      * @throws NullPointerException If a null List is passed.
      * @throws IndexOutOfBoundsException If an empty List is passed.
      */
