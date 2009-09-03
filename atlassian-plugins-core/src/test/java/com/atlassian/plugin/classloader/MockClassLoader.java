5390c564bb7e388bd73778cda346df3b9866315f
mlassau <mlassau@atlassian.com>
2009-09-03 05:21:02 +0000

    PLUG-431 Unit test for new PluginsClassLoader method
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@43157 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugins-core/src/test/java/com/atlassian/plugin/classloader/MockClassLoader.java
@@ -0,0 +1,30 @@
+package com.atlassian.plugin.classloader;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MockClassLoader extends AbstractClassLoader
+{
+    private Map<String, Class> registeredClasses = new HashMap<String, Class>();
+
+    protected URL findResource(final String name)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    protected Class findClass(final String className) throws ClassNotFoundException
+    {
+        Class clazz = registeredClasses.get(className);
+        if (clazz == null)
+        {
+            throw new ClassNotFoundException("Class '" + className + "' not found.");
+        }
+        return clazz;
+    }
+
+    public void register(final String className, final Class clazz)
+    {
+        registeredClasses.put(className, clazz);
+    }
+}
