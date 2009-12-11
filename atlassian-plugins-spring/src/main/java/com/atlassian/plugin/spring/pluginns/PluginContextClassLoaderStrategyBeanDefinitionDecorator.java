fbe574e7fa2b6d06b074448b99b9af49f943c275
sleberrigaud <sleberrigaud@atlassian.com>
2009-12-11 05:16:47 +0000

    PLUG-500: Backported to 2.3.x branch
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/atlassian-plugins-2.3.x@49806 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugins-spring/src/main/java/com/atlassian/plugin/spring/pluginns/PluginContextClassLoaderStrategyBeanDefinitionDecorator.java
@@ -0,0 +1,54 @@
+package com.atlassian.plugin.spring.pluginns;
+
+import com.atlassian.plugin.osgi.hostcomponents.ContextClassLoaderStrategy;
+import com.atlassian.plugin.spring.SpringHostComponentProviderBeanDefinitionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.xml.BeanDefinitionDecorator;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Node;
+
+/**
+ * Processes an "ccls" (Context Class Loader Strategy) attribute in the plugin namespace.
+ * Also handles registering the {@link com.atlassian.plugin.osgi.hostcomponents.HostComponentProvider} through
+ * the {@link com.atlassian.plugin.spring.SpringHostComponentProviderFactoryBean}.
+ *
+ * In the case of hierarchical contexts we will put the host component provider in the lowest possible context.
+ */
+public class PluginContextClassLoaderStrategyBeanDefinitionDecorator implements BeanDefinitionDecorator
+{
+    private final static Log log = LogFactory.getLog(PluginContextClassLoaderStrategyBeanDefinitionDecorator.class);
+
+    /**
+     * Called when the Spring parser encounters an "available" attribute.
+     * @param source The attribute
+     * @param holder The containing bean definition
+     * @param ctx The parser context
+     * @return The containing bean definition
+     */
+    public BeanDefinitionHolder decorate(Node source, BeanDefinitionHolder holder, ParserContext ctx)
+    {
+        final String contextClassLoaderStrategy = ((Attr) source).getValue();
+        if (contextClassLoaderStrategy != null)
+        {
+            SpringHostComponentProviderBeanDefinitionUtils.addContextClassLoaderStrategy(ctx.getRegistry(), holder.getBeanName(), getContextClassLoaderStrategy(contextClassLoaderStrategy));
+        }
+        return holder;
+
+    }
+
+    private ContextClassLoaderStrategy getContextClassLoaderStrategy(String contextClassLoaderStrategy)
+    {
+        try
+        {
+            return ContextClassLoaderStrategy.valueOf(contextClassLoaderStrategy);
+        }
+        catch (IllegalArgumentException e)
+        {
+            log.warn("Cannot parse '" + contextClassLoaderStrategy + "' to a valid context class loader strategy, will use default '" + ContextClassLoaderStrategy.USE_HOST + "'");
+            return ContextClassLoaderStrategy.USE_HOST;
+        }
+    }
+}
