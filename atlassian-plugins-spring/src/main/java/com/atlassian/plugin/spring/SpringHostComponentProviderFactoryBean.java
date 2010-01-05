c053f3361188672b8c4ade41f4fac22a111b918e
sleberrigaud <sleberrigaud@atlassian.com>
2010-01-05 03:09:22 +0000

    PLUG-505: Fixed logging for non singleton beans in SpringHostComponentProviderFactoryBean
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@50880 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-spring/src/main/java/com/atlassian/plugin/spring/SpringHostComponentProviderFactoryBean.java
+++ atlassian-plugins-spring/src/main/java/com/atlassian/plugin/spring/SpringHostComponentProviderFactoryBean.java
@@ -168,12 +168,13 @@ public class SpringHostComponentProviderFactoryBean extends AbstractFactoryBean
                 {
                     try
                     {
-                        if (beanFactory.isSingleton(beanName))
+                        final Class beanClass = getBeanClass(beanFactory.getBean(beanName));
+                        final AvailableToPlugins annotation = AnnotationUtils.findAnnotation(beanClass, AvailableToPlugins.class);
+                        if (annotation != null)
                         {
-                            final Class beanClass = getBeanClass(beanFactory.getBean(beanName));
-                            final AvailableToPlugins annotation = AnnotationUtils.findAnnotation(beanClass, AvailableToPlugins.class);
-                            if (annotation != null)
+                            if (beanFactory.isSingleton(beanName))
                             {
+
                                 beansToProvide.add(beanName);
                                 if (annotation.value() != Void.class) // an interface is defined in the annotation
                                 {
@@ -198,7 +199,7 @@ public class SpringHostComponentProviderFactoryBean extends AbstractFactoryBean
                             }
                             else
                             {
-                                log.warn("Cannot make bean '{}' available to plugins as it is not scoped 'singleton'", beanName);
+                                log.warn("Could not make bean '{}' available to plugins as it is not scoped 'singleton'", beanName);
                             }
                         }
                     }
