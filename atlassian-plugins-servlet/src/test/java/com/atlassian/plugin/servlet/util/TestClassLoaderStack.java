1124ae183dd5a40b912d700d2efeb5eae0f7893c
mlassau <mlassau@atlassian.com>
2010-01-25 01:26:45 +0000

    PLUG-515 Temporarily go back to the original Context ClassLoader to work around issues in Weblogic session attributes.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@53633 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-servlet/src/test/java/com/atlassian/plugin/servlet/util/TestClassLoaderStack.java
+++ atlassian-plugins-servlet/src/test/java/com/atlassian/plugin/servlet/util/TestClassLoaderStack.java
@@ -1,13 +1,8 @@
 package com.atlassian.plugin.servlet.util;
 
-import static com.atlassian.plugin.test.PluginTestUtils.FILTER_TEST_JAR;
-import static com.atlassian.plugin.test.PluginTestUtils.getFileForResource;
+import junit.framework.TestCase;
 
 import java.net.URISyntaxException;
-
-import com.atlassian.plugin.classloader.PluginClassLoader;
-
-import junit.framework.TestCase;
 
 public class TestClassLoaderStack extends TestCase
 {
@@ -16,15 +11,15 @@ public class TestClassLoaderStack extends TestCase
         ClassLoader mainLoader = Thread.currentThread().getContextClassLoader();
         try
         {
-            ClassLoader pluginLoader = new PluginClassLoader(getFileForResource(FILTER_TEST_JAR));
-            ClassLoader pluginLoader2 = new PluginClassLoader(getFileForResource(FILTER_TEST_JAR));
+            ClassLoader pluginLoader1 = new MockClassLoader();
+            ClassLoader pluginLoader2 = new MockClassLoader();
 
-            ClassLoaderStack.push(pluginLoader);
-            assertSame(pluginLoader, Thread.currentThread().getContextClassLoader());
+            ClassLoaderStack.push(pluginLoader1);
+            assertSame(pluginLoader1, Thread.currentThread().getContextClassLoader());
             ClassLoaderStack.push(pluginLoader2);
             assertSame(pluginLoader2, Thread.currentThread().getContextClassLoader());
             ClassLoaderStack.pop();
-            assertSame(pluginLoader, Thread.currentThread().getContextClassLoader());
+            assertSame(pluginLoader1, Thread.currentThread().getContextClassLoader());
             ClassLoaderStack.pop();
             assertSame(mainLoader, Thread.currentThread().getContextClassLoader());
         }
@@ -32,6 +27,54 @@ public class TestClassLoaderStack extends TestCase
         {
             Thread.currentThread().setContextClassLoader(mainLoader);
         }
+    }
 
+    public void testPopReturnsPreviousContextClassLoader() throws Exception
+    {
+        ClassLoader mainLoader = Thread.currentThread().getContextClassLoader();
+        try
+        {
+            ClassLoader pluginLoader1 = new MockClassLoader();
+            ClassLoader pluginLoader2 = new MockClassLoader();
+
+            ClassLoaderStack.push(pluginLoader1);
+            assertSame(pluginLoader1, Thread.currentThread().getContextClassLoader());
+            ClassLoaderStack.push(pluginLoader2);
+            assertSame(pluginLoader2, Thread.currentThread().getContextClassLoader());
+            ClassLoader previous = ClassLoaderStack.pop();
+            assertSame(pluginLoader2, previous);
+            assertSame(pluginLoader1, Thread.currentThread().getContextClassLoader());
+            previous = ClassLoaderStack.pop();
+            assertSame(pluginLoader1, previous);
+            assertSame(mainLoader, Thread.currentThread().getContextClassLoader());
+        }
+        finally
+        {
+            // Clean up in case of error
+            Thread.currentThread().setContextClassLoader(mainLoader);
+        }
+    }
+
+    public void testPushAndPopHandleNull() throws Exception
+    {
+        ClassLoader mainLoader = Thread.currentThread().getContextClassLoader();
+        try
+        {
+            // popping with empty stack should return null
+            assertNull(ClassLoaderStack.pop());
+            // pushing null should be a no-op
+            ClassLoaderStack.push(null);
+            assertSame(mainLoader, Thread.currentThread().getContextClassLoader());
+            assertNull(ClassLoaderStack.pop());
+        }
+        finally
+        {
+            // Clean up in case of error
+            Thread.currentThread().setContextClassLoader(mainLoader);
+        }
+    }
+
+    public static class MockClassLoader extends ClassLoader
+    {
     }
 }
