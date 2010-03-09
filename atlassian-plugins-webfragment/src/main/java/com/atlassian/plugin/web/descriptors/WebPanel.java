043ad627f28bc56b7ac971d9ead64952747a8d2c
evzijst <evzijst@atlassian.com>
2010-03-09 23:36:42 +0000

    PLUG-514 - web-panel plugin module type Rework from CR-PLUG-173.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@56522 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-webfragment/src/main/java/com/atlassian/plugin/web/descriptors/WebPanel.java
+++ atlassian-plugins-webfragment/src/main/java/com/atlassian/plugin/web/descriptors/WebPanel.java
@@ -2,7 +2,24 @@ package com.atlassian.plugin.web.descriptors;
 
 import java.util.Map;
 
-public interface WebPanel {
-
+/**
+ * The module that is responsive for providing the raw content for a we panel.
+ * Whatever is returned by {@link #getHtml(java.util.Map)} is inserted into the
+ * host application's page, so it has to be valid HTML.
+ *
+ * @see {@link DefaultWebPanelModuleDescriptor#getModule()}
+ * @since   2.5.0
+ */
+public interface WebPanel
+{
+    /**
+     * Returns the HTML that will be placed in the host application's page.
+     *
+     * @param context   the contextual information that can be used during
+     *  rendering. Context elements are not standardized and are
+     *  application-specific, so refer to your application's documentation to
+     *  learn what is available.
+     * @return  the HTML that will be placed in the host application's page.
+     */
     String getHtml(Map<String, Object> context);
 }
