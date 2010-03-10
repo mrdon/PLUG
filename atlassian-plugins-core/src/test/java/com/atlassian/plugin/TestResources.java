724d5547f85b68f5ee74f595b0529c671c785b71
jwesleysmith <jwesleysmith@atlassian.com>
2010-03-10 05:31:35 +0000

    PLUG-537 Performance fix. Don't use getResourceDescriptors(String type) as it copies the underlying list. Test that it doesn't like null anymore.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@56555 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/test/java/com/atlassian/plugin/TestResources.java
+++ atlassian-plugins-core/src/test/java/com/atlassian/plugin/TestResources.java
@@ -1,27 +1,26 @@
 package com.atlassian.plugin;
 
-import com.atlassian.plugin.elements.ResourceDescriptor;
-import com.atlassian.plugin.elements.ResourceLocation;
+import java.util.List;
+
 import junit.framework.TestCase;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 
-import java.util.List;
+import com.atlassian.plugin.elements.ResourceDescriptor;
+import com.atlassian.plugin.elements.ResourceLocation;
 
 public class TestResources extends TestCase
 {
-    private static final String RESOURCE_DOC = "<foo>" +
-                    "<resource type=\"velocity\" name=\"view\">the content</resource>" +
-                    "<resource type=\"velocity\" name=\"edit\" />" +
-                    "<resource type=\"image\" name=\"view\" />" +
-                    "</foo>";
+    private static final String RESOURCE_DOC = "<foo>" + "<resource type=\"velocity\" name=\"view\">the content</resource>" + "<resource type=\"velocity\" name=\"edit\" />"
+        + "<resource type=\"image\" name=\"view\" />" + "</foo>";
 
     public void testMultipleResources() throws DocumentException, PluginParseException
     {
-        Resources resources = makeTestResources();
+        final Resources resources = makeTestResources();
 
-        List descriptors = resources.getResourceDescriptors();
+        final List descriptors = resources.getResourceDescriptors();
         assertEquals(3, descriptors.size());
 
         assertDescriptorMatches((ResourceDescriptor) descriptors.get(0), "velocity", "view");
@@ -31,21 +30,32 @@ public class TestResources extends TestCase
 
     public void testGetResourceDescriptorsByType() throws DocumentException, PluginParseException
     {
-        Resources resources = makeTestResources();
+        final Resources resources = makeTestResources();
 
-        assertEquals(0, resources.getResourceDescriptors(null).size());
         assertEquals(0, resources.getResourceDescriptors("blah").size());
 
-        List velocityResources = resources.getResourceDescriptors("velocity");
+        final List velocityResources = resources.getResourceDescriptors("velocity");
         assertEquals(2, velocityResources.size());
 
         assertDescriptorMatches((ResourceDescriptor) velocityResources.get(0), "velocity", "view");
         assertDescriptorMatches((ResourceDescriptor) velocityResources.get(1), "velocity", "edit");
     }
 
+    public void testNullTypeThrows() throws PluginParseException, DocumentException
+    {
+        final Resources resources = makeTestResources();
+        try
+        {
+            resources.getResourceDescriptors(null);
+            fail("IllegalArgumentException expected");
+        }
+        catch (final IllegalArgumentException expected)
+        {}
+    }
+
     public void testGetResourceDescriptor() throws DocumentException, PluginParseException
     {
-        Resources resources = makeTestResources();
+        final Resources resources = makeTestResources();
 
         assertNull(resources.getResourceLocation("image", "edit"));
         assertNull(resources.getResourceLocation("fish", "view"));
@@ -58,10 +68,8 @@ public class TestResources extends TestCase
 
     public void testMultipleResourceWithClashingKeysFail() throws DocumentException
     {
-        Document document = DocumentHelper.parseText("<foo>" +
-                "<resource type=\"velocity\" name=\"view\">the content</resource>" +
-                "<resource type=\"velocity\" name=\"view\" />" +
-                "</foo>");
+        final Document document = DocumentHelper.parseText("<foo>" + "<resource type=\"velocity\" name=\"view\">the content</resource>"
+            + "<resource type=\"velocity\" name=\"view\" />" + "</foo>");
 
         try
         {
@@ -69,7 +77,7 @@ public class TestResources extends TestCase
             Resources.fromXml(document.getRootElement());
             fail("Should have thrown exception about duplicate resources.");
         }
-        catch (PluginParseException e)
+        catch (final PluginParseException e)
         {
             assertEquals("Duplicate resource with type 'velocity' and name 'view' found", e.getMessage());
         }
@@ -82,26 +90,25 @@ public class TestResources extends TestCase
             Resources.fromXml(null);
             fail("Expected exception when parsing null element");
         }
-        catch (IllegalArgumentException expected)
-        {
-        }
+        catch (final IllegalArgumentException expected)
+        {}
     }
 
     public void testEmptyResources() throws Exception
     {
-        Resources resources = Resources.EMPTY_RESOURCES;
+        final Resources resources = Resources.EMPTY_RESOURCES;
         assertTrue("Empty resources should be empty", resources.getResourceDescriptors().isEmpty());
         assertTrue("Empty resources should be empty by type", resources.getResourceDescriptors("i18n").isEmpty());
         assertNull("Empty resources should return null for any resource", resources.getResourceLocation("i18n", "i18n.properties"));
     }
 
-    private void assertLocationMatches(ResourceLocation first, String type, String name)
+    private void assertLocationMatches(final ResourceLocation first, final String type, final String name)
     {
         assertEquals(type, first.getType());
         assertEquals(name, first.getName());
     }
 
-    private void assertDescriptorMatches(ResourceDescriptor first, String type, String name)
+    private void assertDescriptorMatches(final ResourceDescriptor first, final String type, final String name)
     {
         assertEquals(type, first.getType());
         assertEquals(name, first.getName());
@@ -109,7 +116,7 @@ public class TestResources extends TestCase
 
     private Resources makeTestResources() throws DocumentException, PluginParseException
     {
-        Document document = DocumentHelper.parseText(RESOURCE_DOC);
+        final Document document = DocumentHelper.parseText(RESOURCE_DOC);
         return Resources.fromXml(document.getRootElement());
     }
 }
