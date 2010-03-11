979b0eb298c394099c1f85e999b62f2e0e7c3f82
jwesleysmith <jwesleysmith@atlassian.com>
2010-03-11 02:56:13 +0000

    use the atlassian-util-concurrent Timeout class
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@56603 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/WaitUntil.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/WaitUntil.java
@@ -1,10 +1,13 @@
 package com.atlassian.plugin.util;
 
+import static java.lang.Thread.sleep;
+
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static java.lang.Thread.sleep;
-import java.util.concurrent.TimeUnit;
+import com.atlassian.util.concurrent.Timeout;
 
 /**
  * Utility methods for synchronising on asynchronous processes
@@ -19,8 +22,9 @@ public class WaitUntil
     private static final int DEFAULT_STARTUP_WAIT = 60;
 
     /**
-     * Number of seconds to wait before a plugin starts up. Configurable through system property
-     * "com.atlassian.plugin.startup.wait". Defaults to {@link #DEFAULT_STARTUP_WAIT}
+     * Number of seconds to wait before a plugin starts up. Configurable through
+     * system property "com.atlassian.plugin.startup.wait". Defaults to
+     * {@link #DEFAULT_STARTUP_WAIT}
      */
     private static final int STARTUP_WAIT = Integer.getInteger("com.atlassian.plugin.startup.wait", DEFAULT_STARTUP_WAIT);
 
@@ -29,7 +33,7 @@ public class WaitUntil
 
     /**
      * Invokes the wait condition, trying every second for 10 seconds
-     *
+     * 
      * @param waitCondition The condition that determines when to stop waiting
      * @return True if the condition returned true
      */
@@ -39,8 +43,9 @@ public class WaitUntil
     }
 
     /**
-     * Invokes the wait condition, trying every second for the configured seconds
-     *
+     * Invokes the wait condition, trying every second for the configured
+     * seconds
+     * 
      * @param waitCondition The condition that determines when to stop waiting
      * @param tries The number of tries to attempt
      * @return True if the condition returned true
@@ -52,12 +57,14 @@ public class WaitUntil
     }
 
     /**
-     * Invokes the wait condition, trying every second for the configured seconds
-     *
+     * Invokes the wait condition, trying every second for the configured
+     * seconds
+     * 
      * @param waitCondition The condition that determines when to stop waiting
      * @param time the amount of time to wait
      * @param unit the time unit time is specified in
-     * @param retryInterval how often to re-check the condition (specified in the supplied TimeUnit)
+     * @param retryInterval how often to re-check the condition (specified in
+     *            the supplied TimeUnit)
      * @return True if the condition returned true
      */
     public static boolean invoke(final WaitCondition waitCondition, final int time, final TimeUnit unit, final int retryInterval)
@@ -74,7 +81,7 @@ public class WaitUntil
 
             if (log.isInfoEnabled())
             {
-                log.info(waitCondition.getWaitMessage() + ", " + timeout.getRemaining());
+                log.info(waitCondition.getWaitMessage() + ", " + TimeUnit.SECONDS.convert(timeout.getTime(), timeout.getUnit()) + " seconds remaining");
             }
             try
             {
@@ -95,12 +102,14 @@ public class WaitUntil
     {
         /**
          * If the condition has been finished
+         * 
          * @return True if finished and should stop waiting
          */
         boolean isFinished();
 
         /**
          * Gets the wait message to log for each try
+         * 
          * @return The string to print describing why the code is waiting
          */
         String getWaitMessage();
