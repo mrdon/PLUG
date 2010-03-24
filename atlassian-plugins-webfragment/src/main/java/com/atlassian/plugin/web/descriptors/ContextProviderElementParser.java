8d2380063f0777307b9cb8adc10a912c9c98af4d
jwesleysmith <jwesleysmith@atlassian.com>
2010-03-24 04:31:21 +0000

    PLUG-569 use the correct Element when parsing the ContextProvider
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@57234 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-webfragment/src/main/java/com/atlassian/plugin/web/descriptors/ContextProviderElementParser.java
+++ atlassian-plugins-webfragment/src/main/java/com/atlassian/plugin/web/descriptors/ContextProviderElementParser.java
@@ -1,51 +1,53 @@
 package com.atlassian.plugin.web.descriptors;
 
+import org.dom4j.Element;
+
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.loaders.LoaderUtils;
 import com.atlassian.plugin.web.ContextProvider;
 import com.atlassian.plugin.web.WebFragmentHelper;
 import com.atlassian.plugin.web.conditions.ConditionLoadingException;
-import org.dom4j.Element;
 
 /**
- * This class is used for constructing {@link com.atlassian.plugin.web.ContextProvider}
- * objects from a module descriptor's XML element. Its functionality is used
- * by both {@link com.atlassian.plugin.web.descriptors.AbstractWebFragmentModuleDescriptor}
- * and {@link com.atlassian.plugin.web.descriptors.DefaultWebPanelModuleDescriptor}.
- *
- * @since   2.5.0
+ * This class is used for constructing
+ * {@link com.atlassian.plugin.web.ContextProvider} objects from a module
+ * descriptor's XML element. Its functionality is used by both
+ * {@link com.atlassian.plugin.web.descriptors.AbstractWebFragmentModuleDescriptor}
+ * and
+ * {@link com.atlassian.plugin.web.descriptors.DefaultWebPanelModuleDescriptor}.
+ * 
+ * @since 2.5.0
  */
 class ContextProviderElementParser
 {
     private final WebFragmentHelper webFragmentHelper;
 
-    public ContextProviderElementParser(WebFragmentHelper webFragmentHelper)
+    public ContextProviderElementParser(final WebFragmentHelper webFragmentHelper)
     {
         this.webFragmentHelper = webFragmentHelper;
     }
 
     /**
-     *
-     * @param element   the module descriptor's XML element which contains the nested &lt;context-provider> element.
-     * @return  the configured {@link ContextProvider} instance, or <code>null</code> when the descriptor does not
-     * contain a &lt;context-provider> element.
+     * @param element the module descriptor's XML element which contains the
+     *            nested &lt;context-provider> element.
+     * @return the configured {@link ContextProvider} instance, or
+     *         <code>null</code> when the descriptor does not contain a
+     *         &lt;context-provider> element.
      * @throws PluginParseException
      */
-    public ContextProvider makeContextProvider(Plugin plugin, final Element element) throws PluginParseException
+    public ContextProvider makeContextProvider(final Plugin plugin, final Element element) throws PluginParseException
     {
         try
         {
-            if (element.element("context-provider") != null)
-            {
-                final ContextProvider context = webFragmentHelper.loadContextProvider(element.attributeValue("class"), plugin);
-                context.init(LoaderUtils.getParams(element));
-                return context;
-            }
-            else
+            final Element contextProviderElement = element.element("context-provider");
+            if (contextProviderElement == null)
             {
                 return null;
             }
+            final ContextProvider context = webFragmentHelper.loadContextProvider(contextProviderElement.attributeValue("class"), plugin);
+            context.init(LoaderUtils.getParams(element));
+            return context;
         }
         catch (final ClassCastException e)
         {
