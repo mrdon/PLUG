a686e280d042279d1e46a1c8ba851ffb19af8b63
dbrown <dbrown@atlassian.com>
2010-07-26 01:08:17 +0000

    PLUG-620: Fix event publisher to use actual impl
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@64504 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-refimpl/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
+++ atlassian-plugins-refimpl/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
@@ -1,6 +1,15 @@
 package com.atlassian.plugin.refimpl;
 
 import com.atlassian.event.api.EventPublisher;
+import com.atlassian.event.config.EventThreadPoolConfiguration;
+import com.atlassian.event.config.ListenerHandlersConfiguration;
+import com.atlassian.event.internal.AsynchronousAbleEventDispatcher;
+import com.atlassian.event.internal.EventExecutorFactoryImpl;
+import com.atlassian.event.internal.EventPublisherImpl;
+import com.atlassian.event.internal.EventThreadPoolConfigurationImpl;
+import com.atlassian.event.internal.ListenerHandlerConfigurationImpl;
+import com.atlassian.event.spi.EventDispatcher;
+import com.atlassian.event.spi.EventExecutorFactory;
 import com.atlassian.plugin.DefaultModuleDescriptorFactory;
 import com.atlassian.plugin.ModuleDescriptorFactory;
 import com.atlassian.plugin.PluginAccessor;
@@ -149,7 +158,10 @@ public class ContainerManager
         publicContainer = new HashMap<Class<?>, Object>();
         publicContainer.put(PluginController.class, plugins.getPluginController());
         publicContainer.put(PluginAccessor.class, pluginAccessor);
-        publicContainer.put(EventPublisher.class, pluginEventManager);
+
+        // TODO: should re-use event publisher from plugin event manager
+        publicContainer.put(EventPublisher.class, createEventPublisher());
+        
         publicContainer.put(ServletModuleManager.class, servletModuleManager);
         publicContainer.put(WebResourceManager.class, webResourceManager);
         publicContainer.put(Map.class, publicContainer);
@@ -183,6 +195,16 @@ public class ContainerManager
             dir = makeSureDirectoryExists(servletContext, defaultPath);
         }
         return dir;
+    }
+        
+    private EventPublisher createEventPublisher()
+    {
+        EventThreadPoolConfiguration threadPoolConfiguration = new EventThreadPoolConfigurationImpl();
+        EventExecutorFactory factory = new EventExecutorFactoryImpl(threadPoolConfiguration);
+        EventDispatcher dispatcher = new AsynchronousAbleEventDispatcher(factory);
+        ListenerHandlersConfiguration listenerHandlersConfiguration = new ListenerHandlerConfigurationImpl();
+        EventPublisher publisher = new EventPublisherImpl(dispatcher, listenerHandlersConfiguration);
+        return publisher;
     }
 
     private String determineVersion()
