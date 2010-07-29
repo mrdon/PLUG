511d12ca74cd56c1319246a3903e06ae7bfb6cb3
jwesleysmith <jwesleysmith@atlassian.com>
2010-07-29 13:15:51 +0000

    PLUG-599 CR-PLUG-203 Better javadocs.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@64986 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/metadata/ReaderUtil.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/metadata/ReaderUtil.java
@@ -8,7 +8,8 @@ import java.io.Reader;
 import org.apache.commons.io.IOUtils;
 
 /**
- * Utility for managing a using and closing a {@link Reader}.
+ * Utility for managing (reading data from and transforming) and closing a
+ * {@link Reader}.
  * 
  * @since 2.6
  */
@@ -19,12 +20,24 @@ final class ReaderUtil
      * 
      * @param reader to read from, will be closed.
      * @return the lines contained within.
+     * @throws RuntimeException if an IOException is encountered while reading
      */
     static Iterable<String> getLines(final Reader reader)
     {
         return transform(reader, GetRawLines.INSTANCE);
     }
 
+    /**
+     * Transform the contents of a {@link Reader} into some type (T) using a
+     * {@link Transformer}. Closes the reader afterwards and rethrows any
+     * {@link IOException} as an unchecked exception.
+     * 
+     * @param <T> the return type the transformer will create
+     * @param reader the raw character data
+     * @param transformer for transforming the raw data into something
+     * @return the transformer's result
+     * @throws RuntimeException if an IOException is encountered while reading
+     */
     static <T> T transform(final Reader reader, final Transformer<T> transformer)
     {
         try
@@ -41,6 +54,11 @@ final class ReaderUtil
         }
     }
 
+    /**
+     * Given a {@link Reader}, turn it into something.
+     * 
+     * @param <T> the result type
+     */
     interface Transformer<T>
     {
         T get(Reader reader) throws IOException;
