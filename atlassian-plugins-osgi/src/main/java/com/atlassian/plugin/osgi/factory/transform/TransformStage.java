34e5a6bc3dc59a400d265d704441c20d8bf378cc
jwesleysmith <jwesleysmith@atlassian.com>
2010-08-19 05:23:52 +0000

    fixed a spelling error in the javadoc
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@106419 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/factory/transform/TransformStage.java
+++ atlassian-plugins-osgi/src/main/java/com/atlassian/plugin/osgi/factory/transform/TransformStage.java
@@ -1,7 +1,7 @@
 package com.atlassian.plugin.osgi.factory.transform;
 
 /**
- * Performes an stage in the transformation from a JAR to an OSGi bundle
+ * Performs a stage in the transformation from a JAR to an OSGi bundle
  *
  * @since 2.2.0
  */
@@ -16,7 +16,5 @@ public interface TransformStage
      * @param context The transform context to operate on
      * @throws PluginTransformationException If the stage cannot be performed and the whole operation should be aborted
      */
-    void execute(TransformContext context)
-        throws PluginTransformationException;
-
+    void execute(TransformContext context) throws PluginTransformationException;
 }
