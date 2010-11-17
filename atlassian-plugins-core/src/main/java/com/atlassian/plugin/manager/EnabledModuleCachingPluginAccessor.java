cad2389643dcadc98a137a3ad5d5a73ab5f6d508
dbrown <dbrown@atlassian.com>
2010-11-17 06:46:53 +0000

    PLUG-548: Fix strong reference to module descriptor classes
    * Now use weak keys and values in cache and flush after every plugin disable
    * Fix tests so they actually test something :)
    
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@111395 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/manager/EnabledModuleCachingPluginAccessor.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/manager/EnabledModuleCachingPluginAccessor.java
@@ -6,7 +6,9 @@ import static com.google.common.collect.ImmutableList.copyOf;
 
 import com.atlassian.plugin.ModuleDescriptor;
 import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.plugin.event.PluginEventListener;
 import com.atlassian.plugin.event.PluginEventManager;
+import com.atlassian.plugin.event.events.PluginDisabledEvent;
 import com.atlassian.plugin.tracker.PluginModuleTracker;
 
 import com.google.common.base.Function;
@@ -23,18 +25,32 @@ import java.util.concurrent.ConcurrentMap;
 public final class EnabledModuleCachingPluginAccessor extends ForwardingPluginAccessor implements PluginAccessor
 {
     private final PluginEventManager pluginEventManager;
-    private final ConcurrentMap<Class<ModuleDescriptor<Object>>, PluginModuleTracker<Object, ModuleDescriptor<Object>>> cache = new MapMaker().makeComputingMap(new PluginModuleTrackerFactory());
+    private final ConcurrentMap<Class<ModuleDescriptor<Object>>, PluginModuleTracker<Object, ModuleDescriptor<Object>>> cache = new MapMaker()
+            .weakKeys().weakValues().makeComputingMap(new PluginModuleTrackerFactory());
 
     public EnabledModuleCachingPluginAccessor(final PluginAccessor delegate, final PluginEventManager pluginEventManager)
     {
         super(delegate);
         this.pluginEventManager = checkNotNull(pluginEventManager);
+        this.pluginEventManager.register(this);
     }
 
     @Override
     public <D extends ModuleDescriptor<?>> List<D> getEnabledModuleDescriptorsByClass(final Class<D> descriptorClazz)
     {
         return copyOf(descriptors(descriptorClazz));
+    }
+
+    /**
+     * Clears the enabled module cache when any plugin is disabled.  The cache already has weak keys and values, but this
+     * ensures old modules are never returned from disabled plugins.
+     *
+     * @param event The plugin disabled event
+     */
+    @PluginEventListener
+    public void onPluginDisable(PluginDisabledEvent event)
+    {
+        cache.clear();
     }
 
     /**
--- atlassian-plugins-core/src/test/java/com/atlassian/plugin/manager/TestEnabledModuleCachingPluginAccessor.java
+++ atlassian-plugins-core/src/test/java/com/atlassian/plugin/manager/TestEnabledModuleCachingPluginAccessor.java
@@ -1,18 +1,21 @@
 package com.atlassian.plugin.manager;
 
 import com.atlassian.plugin.ModuleDescriptor;
+import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.plugin.event.events.PluginDisabledEvent;
 import com.atlassian.plugin.event.events.PluginModuleEnabledEvent;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class TestEnabledModuleCachingPluginAccessor extends TestDefaultPluginManager
 {
     private PluginAccessor delegate;
-    private PluginAccessor cachingPluginAccessor;
+    private PluginAccessor cachingPluginAccessorToMock;
+    private PluginAccessor cachingPluginAccessorToReal;
 
     @Override
     protected void setUp() throws Exception
@@ -20,40 +23,54 @@ public class TestEnabledModuleCachingPluginAccessor extends TestDefaultPluginMan
         super.setUp();
 
         delegate = Mockito.mock(PluginAccessor.class);
-        cachingPluginAccessor = new EnabledModuleCachingPluginAccessor(manager, pluginEventManager);
+        cachingPluginAccessorToMock = new EnabledModuleCachingPluginAccessor(delegate, pluginEventManager);
+        cachingPluginAccessorToReal = new EnabledModuleCachingPluginAccessor(manager, pluginEventManager);
     }
 
     @Override
     protected PluginAccessor getPluginAccessor()
     {
-        return cachingPluginAccessor;
+        return cachingPluginAccessorToReal;
     }
 
     public void testDelegateShouldCalculateAtMostOnce()
     {
         // call the cached method multiple times.
-        cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
-        cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
 
         // broadcast new module and then call again.
         DummyParentDescriptor descriptor = mock(DummyParentDescriptor.class);
         pluginEventManager.broadcast(new PluginModuleEnabledEvent(descriptor));
-        cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
 
         // should have been called at most once.
-        verify(delegate, atMost(1)).getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        verify(delegate, times(1)).getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+    }
+
+    public void testFlushCacheAfterAnyPluginDisable()
+    {
+        // call the cached method multiple times.
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+
+        pluginEventManager.broadcast(new PluginDisabledEvent(mock(Plugin.class)));
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+
+        verify(delegate, times(2)).getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
     }
 
     public void testChildAndParentClassBeingTrackedSeparately()
     {
-        cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyChildDescriptor.class);
-        cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyChildDescriptor.class);
+        cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class);
 
         pluginEventManager.broadcast(new PluginModuleEnabledEvent(mock(DummyChildDescriptor.class)));
         pluginEventManager.broadcast(new PluginModuleEnabledEvent(mock(DummyParentDescriptor.class)));
 
-        assertEquals(1, cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyChildDescriptor.class).size());
-        assertEquals(2, cachingPluginAccessor.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class).size());
+        assertEquals(1, cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyChildDescriptor.class).size());
+        assertEquals(2, cachingPluginAccessorToMock.getEnabledModuleDescriptorsByClass(DummyParentDescriptor.class).size());
     }
 
     public abstract class DummyParentDescriptor implements ModuleDescriptor
