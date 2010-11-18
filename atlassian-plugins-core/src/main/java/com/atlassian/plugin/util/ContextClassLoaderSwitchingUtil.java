7082831293133f28f3a46c6e53264c1ae116fd36
jkodumal <jkodumal@atlassian.com>
2010-11-18 18:26:35 +0000

    PLUG-688 : A new runInContext method that takes a Callable
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@111518 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/ContextClassLoaderSwitchingUtil.java
+++ atlassian-plugins-core/src/main/java/com/atlassian/plugin/util/ContextClassLoaderSwitchingUtil.java
@@ -1,5 +1,9 @@
 package com.atlassian.plugin.util;
 
+import java.util.concurrent.Callable;
+
+import static java.util.concurrent.Executors.callable;
+
 /**
  * Utility to run Runnable implementations in a different ClassLoader.
  *
@@ -19,10 +23,34 @@ public class ContextClassLoaderSwitchingUtil
      */
     public static void runInContext(ClassLoader newClassLoader, Runnable runnable)
     {
+        try
+        {
+            runInContext(newClassLoader, callable(runnable));
+        }
+        catch (Exception e)
+        {
+            // Do nothing, this can't happen
+        }
+    }
+    
+    /**
+     * Executes the provided {@link Callable} implementation in the specified {@link ClassLoader}.
+     * <p/>
+     * Utilises the {@link com.atlassian.plugin.util.ClassLoaderStack#push} method to save the old {@link ClassLoader} and
+     * set the one specified as <code>newClassLoader</code>. {@link com.atlassian.plugin.util.ClassLoaderStack#pop} is
+     * called in a finally block to ensure the {@link ClassLoader} is set back to the original one.
+     *
+     * @param newClassLoader The {@link ClassLoader} to run the specified {@link Runnable} in.
+     * @param callable The implementation to be run in the specified {@link ClassLoader}
+     * @return the result of the {@link Callable}
+     * @since 2.7.0
+     */
+    public static <T> T runInContext(ClassLoader newClassLoader, Callable<T> callable) throws Exception
+    {
         ClassLoaderStack.push(newClassLoader);
         try
         {
-            runnable.run();
+            return callable.call();
         }
         finally
         {
