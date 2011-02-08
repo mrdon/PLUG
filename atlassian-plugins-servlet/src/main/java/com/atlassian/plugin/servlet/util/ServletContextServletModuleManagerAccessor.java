3775d7a261b5830cb59d7db263cde7ccdff5df8d
akazatchkov <akazatchkov@atlassian.com>
2011-02-08 00:03:09 +0000

    PLUG-713: don't throw exception if ServletModuleManager is not found in servlet context
    git-svn-id: https://studio.atlassian.com/svn/PLUG/branches/atlassian-plugins-2.7.x@117973 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-servlet/src/main/java/com/atlassian/plugin/servlet/util/ServletContextServletModuleManagerAccessor.java
+++ atlassian-plugins-servlet/src/main/java/com/atlassian/plugin/servlet/util/ServletContextServletModuleManagerAccessor.java
@@ -18,18 +18,11 @@ public class ServletContextServletModuleManagerAccessor
      * Gets the servlet module manager in the servlet context
      *
      * @param servletContext the servlet context to look up the servlet module manager in
-     * @return The servlet module manager instance
-     * @throws IllegalStateException If it hasn't been initialised yet
+     * @return The servlet module manager instance or null if it is not there
      */
     public static ServletModuleManager getServletModuleManager(ServletContext servletContext) throws IllegalStateException
     {
-        ServletModuleManager servletModuleManager = (ServletModuleManager) servletContext.getAttribute(SERVLET_MODULE_MANAGER_KEY);
-        if (servletModuleManager == null)
-        {
-            throw new IllegalStateException("The "+ ServletContextServletModuleManagerAccessor.class.getName()+" has not been " +
-                "set with the current ServletContext.");
-        }
-        return servletModuleManager;
+        return (ServletModuleManager) servletContext.getAttribute(SERVLET_MODULE_MANAGER_KEY);
     }
 
     /**
