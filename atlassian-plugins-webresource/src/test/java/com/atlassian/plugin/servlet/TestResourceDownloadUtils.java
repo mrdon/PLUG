274c1542f305d2a7f9881ec4b9ed57f0501878c2
dwillis <dwillis@atlassian.com>
2011-03-31 12:26:15 +0000

    PLUG-747 Fixed ResourceDownloadUtils to set Expires header a year in the future, as implied by the existing code.
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@121159 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-webresource/src/test/java/com/atlassian/plugin/servlet/TestResourceDownloadUtils.java
+++ atlassian-plugins-webresource/src/test/java/com/atlassian/plugin/servlet/TestResourceDownloadUtils.java
@@ -1,5 +1,6 @@
 package com.atlassian.plugin.servlet;
 
+import com.mockobjects.constraint.Constraint;
 import com.mockobjects.dynamic.Mock;
 import com.mockobjects.dynamic.C;
 
@@ -8,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import junit.framework.TestCase;
 
+import java.util.Date;
+
 public class TestResourceDownloadUtils extends TestCase
 {
     private static final long ONE_YEAR = 60L * 60L * 24L *365L;
+    private static final long ONE_YEAR_MS = ONE_YEAR * 1000;
     private static final String CACHE_CONTROL = "Cache-Control";
 
     public void testAddPublicCachingHeaders()
@@ -18,21 +22,43 @@ public class TestResourceDownloadUtils extends TestCase
         Mock mockRequest = new Mock(HttpServletRequest.class);
 
         Mock mockResponse = new Mock(HttpServletResponse.class);
-        mockResponse.expect("setDateHeader", C.ANY_ARGS);
+        mockResponse.expect("setDateHeader", C.args(C.eq("Expires"), aboutNowPlusAYear()));
         mockResponse.expect("setHeader", C.args(C.eq(CACHE_CONTROL), C.eq("max-age=" + ONE_YEAR)));
         mockResponse.expect("addHeader", C.args(C.eq(CACHE_CONTROL), C.eq("public")));
 
         ResourceDownloadUtils.addPublicCachingHeaders((HttpServletRequest) mockRequest.proxy(), (HttpServletResponse) mockResponse.proxy());
+        mockResponse.verify();
     }
 
     public void testAddCachingHeadersWithCacheControls()
     {
         Mock mockResponse = new Mock(HttpServletResponse.class);
-        mockResponse.expect("setDateHeader", C.ANY_ARGS);
+        mockResponse.expect("setDateHeader", C.args(C.eq("Expires"), aboutNowPlusAYear()));
         mockResponse.expect("setHeader", C.args(C.eq(CACHE_CONTROL), C.eq("max-age=" + ONE_YEAR)));
         mockResponse.expect("addHeader", C.args(C.eq(CACHE_CONTROL), C.eq("private")));
         mockResponse.expect("addHeader", C.args(C.eq(CACHE_CONTROL), C.eq("foo")));
 
         ResourceDownloadUtils.addCachingHeaders((HttpServletResponse) mockResponse.proxy(), "private", "foo");
+        mockResponse.verify();
+    }
+
+    private static Constraint aboutNowPlusAYear()
+    {
+        return new Constraint()
+        {
+            private final long nowPlusAYear = System.currentTimeMillis() + ONE_YEAR_MS;
+            private final long nowPlusAYearPlus1s = nowPlusAYear + 1000;
+            public boolean eval(Object o)
+            {
+                long time = (Long) o;
+                return time >= nowPlusAYear && time < nowPlusAYearPlus1s;
+            }
+
+            @Override
+            public String toString()
+            {
+                return "time >= " + nowPlusAYear + " and < " + nowPlusAYearPlus1s;
+            }
+        };
     }
 }
