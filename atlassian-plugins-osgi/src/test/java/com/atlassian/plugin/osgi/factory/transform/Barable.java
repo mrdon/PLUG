3115622c4f166ff1d4ae1aa474e9b25410246e72
psongsiritat <psongsiritat@atlassian.com>
2011-09-01 23:01:42 +0000

    PLUG-636: added more comments
    git-svn-id: https://studio.atlassian.com/svn/PLUG/trunk@129300 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-plugins-osgi/src/test/java/com/atlassian/plugin/osgi/factory/transform/Barable.java
+++ atlassian-plugins-osgi/src/test/java/com/atlassian/plugin/osgi/factory/transform/Barable.java
@@ -4,5 +4,6 @@ import com.atlassian.plugin.osgi.factory.transform.test.SomeClass;
 
 public interface Barable
 {
+    // NOTE: this Integer parameter is used to verify that java.* packages are not imported/exported.
     SomeClass getSomeClass(Integer blah);
 }
